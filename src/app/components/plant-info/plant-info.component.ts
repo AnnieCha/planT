da375b1c6d626ff7ed87a5dea68c383fb276a0e3
AnnieCha <annie.berend@gmx.de>
2022-06-20 23:30:53 +0200

    add layout

--- src/app/components/plant-info/plant-info.component.ts
+++ src/app/components/plant-info/plant-info.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantService } from 'src/app/services/plant.service';
+import { Plant } from 'src/app/shared/models/plant';
 
 @Component({
   selector: 'app-plant-info',
   templateUrl: './plant-info.component.html',
   styleUrls: ['./plant-info.component.scss']
 })
-export class PlantInfoComponent implements OnInit {
 
-  constructor() { }
+export class PlantInfoComponent implements OnInit {
+  public currPlant?: Plant;
+
+  constructor(
+    private _plantService: PlantService
+    ) {}
+
 
   ngOnInit(): void {
+    this.currPlant = this._plantService.getCurrenPlant();
   }
 
 }
--- src/app/shared/models/plant.ts
+++ src/app/shared/models/plant.ts
@@ -1,3 +1,3 @@
 export class Plant {
-    constructor(public plantid: number, public name: string, public infotext: string, public wateramount: number, public sunamount: number, public imageUrl: string){}
+    constructor(public plantid: number, public name: string, public infotext: string, public longtext: string, public wateramount: number, public sunamount: number, public imageUrl: string){}
 }
