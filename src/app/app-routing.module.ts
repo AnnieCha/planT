1252d3e678be9fa284e870f8f7c7bc357a940e7b
AnnieCha <annie.berend@gmx.de>
2022-06-21 10:40:42 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlantDetailComponent } from './components/plant-detail/plant-detail.component';
+import { PlantInfoComponent } from './components/plant-info/plant-info.component';
+import { PlantlistComponent } from './components/plantlist/plantlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'plant/:name', component: PlantInfoComponent },
+  { path: '', component: PlantlistComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/plant-info/plant-info.component.ts
+++ src/app/components/plant-info/plant-info.component.ts
@@ -1,4 +1,6 @@
+import { partitionArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PlantService } from 'src/app/services/plant.service';
 import { Plant } from 'src/app/shared/models/plant';
 
@@ -10,14 +12,25 @@ import { Plant } from 'src/app/shared/models/plant';
 
 export class PlantInfoComponent implements OnInit {
   public currPlant?: Plant;
+  private sub: any;
+  private plantName: string = "";
 
   constructor(
-    private _plantService: PlantService
+    private _plantService: PlantService,
+    private route: ActivatedRoute
     ) {}
 
 
   ngOnInit(): void {
-    this.currPlant = this._plantService.getCurrenPlant();
+ //   this.currPlant = this._plantService.getCurrenPlant();
+    this.sub = this.route.params.subscribe(params => {
+      this.plantName = params['name'];
+    })
+    this.getPlantInfo();
+  }
+
+  public getPlantInfo(){
+    this.currPlant = this._plantService.getCurrenPlant(this.plantName);
   }
 
 }
