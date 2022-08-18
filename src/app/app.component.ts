3339662144899afbff9edc0910fe37e7bdb83185
NormsChamp <bulletnorm@googlemail.com>
2022-08-18 16:49:58 +0200

    designkrams
    designkrams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import {UserService} from 'src/app/services/user.service';
+=======
+import { TranslateService } from '@ngx-translate/core';
+import { UserService } from 'src/app/services/user.service';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
@@ -9,9 +14,43 @@ import {UserService} from 'src/app/services/user.service';
 export class AppComponent {
   title = 'plant-app';
 
+<<<<<<< Updated upstream
   constructor(private _userService: UserService) {
   }
 
   
+=======
+
+  constructor(private _translateService: TranslateService, private _userService: UserService) {
+    _translateService.setDefaultLang('en');
+    _translateService.use('en');
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  chooseLanguage(lang: string) {
+    this._translateService.use(lang);
+  }
+
+  showMenue() {
+    return this._userService.getLoggedIn()
+  }
+
+  onBtnClick = () => {
+    document.body.classList.toggle("dark-theme");
+    console.log("darkThemeWorks");
+  };
+  /*
+  function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-theme");
+  }
+  
+  */
+
+
+>>>>>>> Stashed changes
 }
 
