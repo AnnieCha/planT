4d0bb96291c583f0709e578b05eda05ad324d72c
AnnieCha <annie.berend@gmx.de>
2022-08-18 11:14:39 +0200

    change build script

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import { plantRouter } from "./server";
 const app = express();
 const port = 5200; // default port to listen
 const cors = require('cors');
+const serverless = require('serverless-http');
 
 //const dotenv = require('dotenv');
 //dotenv.config({ path: path.resolve(__dirname, './env') });
@@ -12,11 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+app.use(plantRouter);
+
 //const PORT = process.env.PORT || 5200;
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 })
 
-app.use(plantRouter);
-
 module.exports = app;
+module.exports.handler = serverless(app);
