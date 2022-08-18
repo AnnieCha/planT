acd9060322850efaaa087cb6928476f457e61d7c
AnnieCha <annie.berend@gmx.de>
2022-08-18 12:06:28 +0200

    install netlify lambda and functions

--- app.ts
+++ app.ts
@@ -9,11 +9,13 @@ const serverless = require('serverless-http');
 //const dotenv = require('dotenv');
 //dotenv.config({ path: path.resolve(__dirname, './env') });
 
+const apiRoute = '/.netlify/functions/server/plantapp';
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
-app.use(plantRouter);
+app.use(apiRoute,plantRouter);
 
 //const PORT = process.env.PORT || 5200;
 app.listen(port, () => {
